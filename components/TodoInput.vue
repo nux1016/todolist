6118c93e83c35bfc1009b0945be24160896abd91
nux1016 <105186993+nux1016@users.noreply.github.com>
2022-05-18 17:53:03 +0900

    add -> 추가

--- components/TodoInput.vue
+++ components/TodoInput.vue
@@ -3,6 +3,6 @@
 <template>
   <div class="add">
     <input type="text" class="add__input" placeholder="Enter your task" />
-    <button class="add__button">Add</button>
+    <button class="add__button">추가</button>
   </div>
 </template>
