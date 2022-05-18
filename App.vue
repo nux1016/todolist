90a2ba095a82fedaeee83d102e7d276f562330b3
nux1016 <105186993+nux1016@users.noreply.github.com>
2022-05-18 17:45:22 +0900

    first commit

---
+++ App.vue
@@ -0,0 +1,33 @@
+<!-- App.vue -->
+ 
+<template>
+  <div id="app">
+    <TodoHeader />
+    <TodoTitle />
+    <TodoInput />
+    <TodoController />
+    <TodoList />
+    <TodoFooter />
+  </div>
+</template>
+ 
+<script>
+import TodoHeader from "./components/TodoHeader";
+import TodoTitle from "./components/TodoTitle";
+import TodoInput from "./components/TodoInput";
+import TodoController from "./components/TodoController";
+import TodoList from "./components/TodoList";
+import TodoFooter from "./components/TodoFooter";
+ 
+export default {
+  name: "App",
+  components: {
+    TodoHeader,
+    TodoTitle,
+    TodoInput,
+    TodoController,
+    TodoList,
+    TodoFooter
+  }
+};
+</script>
---
+++ components/TodoFooter.vue
@@ -0,0 +1,5 @@
+<!-- TodoFooter.vue -->
+ 
+<template>
+  <footer class="footer">열심히 하자</footer>
+</template>
